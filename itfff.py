d241ccdb30ab264a4f20e26e4f8bb9eebb86af2c
WayneJiang <LibraFaith@gmail.com>
2022-03-02 20:24:11 +0800

    Update itfff.py

--- itfff.py
+++ itfff.py
@@ -6,7 +6,7 @@ def Line(msg):
     url = ('https://maker.ifttt.com/trigger/update/with/key/dRpC2gm9MCzrCHayYmTaq8?value1='+str(msg))
     r = requests.get(url)
     if r.text[:5] == 'Congr':  
-        print('成功推送 (' +str(msg)+') 至 Line')
+        print('成功推送至 Line')
     return r.text
 
 def GetPrice(symbol):
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     while True:
         btc = GetPrice('BTCUSDT')
         eth = GetPrice('ETHUSDT')
-        msg = '\n比特幣即時價格 ： ' + str(btc) + '美元' + '\n乙太幣即時價格 ： ' + str(eth) + '美元'
+        msg = '\n比特幣即時價格 ： ' + str(btc) + ' 美元' + '\n乙太幣即時價格 ： ' + str(eth) + ' 美元'
         Line(msg)
         time.sleep(600)
