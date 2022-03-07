397e4d29f53f1dff06ed37e875cf7fcd8a1a759a
WayneJiang <LibraFaith@gmail.com>
2022-03-07 16:56:20 +0800

    Update itfff.py

--- itfff.py
+++ itfff.py
@@ -28,13 +28,15 @@ def GetKLine(symbol):
     col_open_time, col_open, col_high, col_low, col_close, col_volume, col_close_time = [], [], [], [], [], [], []
     
     for data in json:
-        col_open_time.append(datetime.datetime.fromtimestamp(json[0]/1000))
+        time = json[0]
+        col_open_time.append(datetime.datetime.fromtimestamp(time/1000))
         col_open.append(json[1])
         col_high.append(json[2])
         col_low.append(json[3])
         col_close.append(json[4])
         col_volume.append(json[5])
-        col_close_time.append(datetime.datetime.fromtimestamp(json[6]/1000))
+        time = json[6]
+        col_close_time.append(datetime.datetime.fromtimestamp(time/1000))
         
     data_frame['Open Time'] = col_open_time
     data_frame['Open'] = numpy.array(col_open).astype(numpy.float)
